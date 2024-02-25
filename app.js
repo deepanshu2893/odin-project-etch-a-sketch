983f70d344182eda16f4b68954d2754852712f6f
Deepanshu Panwar <deepanshu2893@gmail.com>
2024-02-25 22:00:29 +0530

    etch a sketch

---
+++ app.js
@@ -0,0 +1,8 @@
+const container = document.querySelector("#container");
+
+
+for (let i = 0; i < 16 * 16; i++){
+    const gridDiv = document.createElement("div");
+    gridDiv.classList.add("square");
+    container.appendChild(gridDiv);
+}
