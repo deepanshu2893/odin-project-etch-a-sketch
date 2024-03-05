efac995aa6a00da6c1aa05b617a0d58eb7796759
Deepanshu Panwar <deepanshu2893@gmail.com>
2024-03-05 18:13:30 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const container = document.querySelector("#container");
 
 
-for (let i = 0; i < 16 * 16; i++){
+for (let i = 1; i <= 16 * 16; i++) {
     const gridDiv = document.createElement("div");
     gridDiv.classList.add("square");
     container.appendChild(gridDiv);
